e4af1bce909f3e3a4d15c66ac57acc531890508b
venkata ramana govindu <venkataramanagovindu2015@gmail.com>
2024-03-11 17:11:12 -0700

    before refactor

--- take_home_Q7/MullaNasruddinTales.py
+++ take_home_Q7/MullaNasruddinTales.py
@@ -14,19 +14,14 @@ def read_file_and_devide_tales():
                 if line == '\n':
                     single_tale += line
                 if index > 0:
-                    # print(f'index of . is {index}')
-                    # print(line[0 : index])
-                    # print(line, end='')
                     number_string = line[0 : index]
 
                     if number_string.isdigit():
-                        # print(f"It's a number {number_string}")
                         if single_tale != '':
                             tales_list.append(single_tale)
                         single_tale = line
                     else :
                         single_tale += line
-                        # print(f"It's not a number {number_string}")
             
             if single_tale != '' and '\n':
                 tales_list.append(single_tale)
@@ -38,25 +33,12 @@ def main():
     
     total_number_of_tales = len(tales_list)
 
-    print(f"length of list {total_number_of_tales}")
+    print(f"Total tales we have {total_number_of_tales}")
 
     number = input('Enter number of Tales to retrive ')
 
-    # while number.isdigit() == False or not number == '' :
-    #     number = input(f'Please enter the valid number an integer between (1 & {total_number_of_tales}) ')    
-    #     if number.isdigit() == True:
-    #         number = int(number)
-    #         while number > total_number_of_tales or number == '':
-    #             number = input(f'Please a number less than the total available tales {total_number_of_tales} ') 
-
-    while (not number == '') and not (number.isdigit() == True and 1 <= int(number) < total_number_of_tales):
+    while (not number == '') and not (number.isdigit() == True and 1 <= int(number) <= total_number_of_tales):
         number = input(f'Please enter the valid number an integer between (1 & {total_number_of_tales}) ')    
-        # if number.isdigit() == True:
-        #     number = int(number)
-        #     while number > total_number_of_tales or number == '':
-        #         number = input(f'Please a number less than the total available tales {total_number_of_tales} ') 
-
-  
 
     if number == '':
         number = DEFAULT_NUMBER_OF_TALES
